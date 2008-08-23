2d9c1be6c47563c51f7dc4f6a48f608384587f59
Joon Lee <joon@leelaboratory.com>
2008-08-23 10:54:35 -0400

    added global order vars for author & subject

--- config/environment.rb
+++ config/environment.rb
@@ -67,3 +67,18 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
+
+AUTHOR_ORDER = {
+  "one" => "`authors`.one_total desc, `authors`.one_first desc, `authors`.one_last desc, `authors`.one_middle desc, `authors`.last_name desc",
+  "five" => "`authors`.five_total desc, `authors`.five_first desc, `authors`.five_last desc, `authors`.five_middle desc, `authors`.last_name desc",
+  "ten" => "`authors`.ten_total desc, `authors`.ten_first desc, `authors`.ten_last desc, `authors`.ten_middle desc, `authors`.last_name desc",
+  "all" => "`authors`.all_total desc, `authors`.all_first desc, `authors`.all_last desc, `authors`.all_middle desc, `authors`.last_name desc"
+
+}
+
+SUBJECT_ORDER = {
+  "one" => "`subjects`.one_major desc, `subjects`.one_total desc",
+  "five" => "`subjects`.five_major desc, `subjects`.five_total desc",
+  "ten" => "`subjects`.ten_major desc, `subjects`.ten_total desc",
+  "all" => "`subjects`.all_major desc, `subjects`.all_total desc" 
+}
