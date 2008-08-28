b1d669f8834d2b0086d7dbbe9c1e5f428c7e36ab
Joon Lee <joon@kimlee.org>
2008-08-27 23:44:45 -0400

    minimalism. remove periods.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
   def citation(article, period = 'all')
     journal = content_tag :em, article.journal.to_s
     journal_link = link_to(journal, journal_path(article.journal) + '/' + period)
-    citation = "#{journal_link}. #{article.pubdate.year}." 
+    citation = "#{journal_link} #{article.pubdate.year}" 
     content_tag :span, citation, :class => "citation"
   end
 end
