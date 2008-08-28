6f810069145124eccc12be5c19b6cba711c3e382
Joon Lee <joon@kimlee.org>
2008-08-27 23:52:40 -0400

    make the whole citation box clickable

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,7 @@
 module ArticlesHelper
   def citation(article, period = 'all')
-    journal = content_tag :em, article.journal.to_s
-    journal_link = link_to(journal, journal_path(article.journal) + '/' + period)
-    citation = "#{journal_link} #{article.pubdate.year}" 
+    journal = content_tag(:em, article.journal.to_s)
+    citation = link_to(journal + " #{article.pubdate.year}", journal_path(article.journal) + '/' + period)
     content_tag :span, citation, :class => "citation"
   end
 end
