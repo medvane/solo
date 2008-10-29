0a008af1be2b2aa0c43dfad3b845ab2fcab5ebab
Joon Lee <joon@leelaboratory.com>
2008-10-29 04:05:28 -0400

    cosmetic: show more articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   def index(period = "all")
     total_entries = BibliomeStat.last_cached.send("#{period}_articles")
-    per_page = per_page(total_entries)
+    per_page = per_page(total_entries, 14)
     @articles = Article.search params[:q], :page => params[:page], :per_page => per_page, :total_entries => total_entries
 
     respond_to do |format|
