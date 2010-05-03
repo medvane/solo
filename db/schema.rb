27d5d9f7d9d57134cbfce85d67e49c9124c99e53
Joon Lee <seouri@gmail.com>
2010-05-03 12:51:43 -0400

    upgraded to Rails 2.3.5

--- db/schema.rb
+++ db/schema.rb
@@ -478,11 +478,10 @@ ActiveRecord::Schema.define(:version => 20080818163139) do
     t.string  "issn"
     t.string  "essn"
     t.string  "nlm_id"
-    t.float   "impact_factor"
-    t.integer "one",           :default => 0, :null => false
-    t.integer "five",          :default => 0, :null => false
-    t.integer "ten",           :default => 0, :null => false
-    t.integer "all",           :default => 0, :null => false
+    t.integer "one",    :default => 0, :null => false
+    t.integer "five",   :default => 0, :null => false
+    t.integer "ten",    :default => 0, :null => false
+    t.integer "all",    :default => 0, :null => false
   end
 
   add_index "journals", ["abbr"], :name => "index_journals_on_abbr"
