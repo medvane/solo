5e453774bb54d3385c3640fbca108dec9aab10f5
Joon Lee <seouri@gmail.com>
2010-05-05 21:28:19 -0400

    Turned off page caching because it's also caching in development environment. It seems like a bug in Rails 3 beta3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  caches_page :index, :one, :five, :ten, :all
+  #caches_page :index, :one, :five, :ten, :all
 
   def index(period = "all")
     @bibliome_stat = BibliomeStat.last_cached
