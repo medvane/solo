be82cbd8e456a66f9e064de81b645ff8e8da1c24
Joon Lee <seouri@gmail.com>
2010-09-02 16:13:02 -0400

    pluralize dashboard

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,9 @@ module HomeHelper
     return if bibliome_stat.nil?
     th = []
     for item in %w(articles journals authors subjects genes) do
-      link_text = number_with_delimiter(bibliome_stat.send("#{@period}_#{item}")) + "<br>".html_safe + item.capitalize
+      count = bibliome_stat.send("#{@period}_#{item}")
+      text = count == 1 || count =~ /^1(\.0+)?$/ ? item.singularize : item
+      link_text = number_with_delimiter(count) + "<br>".html_safe + text
       link = link_to(link_text.html_safe, eval("#{@period}_#{item}_path"))
       th.push(content_tag(:th, link.html_safe, :width => "20%", :class => item))
     end
